741172936eb562ed55101c17ad4084bac3b01f61
hussain7482 <tousifh111@gmail.com>
2024-12-05 06:50:19 +0000

    updated smtp for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  .action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp-relay.brevo.com',
+    port: 587,
+    user_name: '814782001@smtp-brevo.com',  # New login email
+    password: 'UbkB0zn1dx8v7jGS',            # New master password
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
